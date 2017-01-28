bbdf2701c36bef015f04986c2147977229005d34
olegnovitskiy <oleg.novitskiy.dev@gmail.com>
2017-01-28 21:29:33 +0200

    Java 7 - Task 7

--- src/main/java/org/store/api/book/BookReaderImpl.java
+++ src/main/java/org/store/api/book/BookReaderImpl.java
@@ -5,7 +5,13 @@ import org.apache.commons.lang3.math.NumberUtils;
 import org.shop.model.Book;
 import org.store.util.DateUtil;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -17,37 +23,38 @@ public class BookReaderImpl implements BookReader {
 	public List<Book> readCsv(String path) {
 		List<Book> books = new ArrayList<>();
 
-		String line = "";
 		String cvsSplitBy = ",";
 		int idx = 0;
 
-		try (InputStream is = this.getClass().getClassLoader().getResourceAsStream(path);
-			 BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(getClass().getClassLoader().getResource(path).toURI()));
+
+            for (String line: lines
+                 ) {
+                idx++;
+                if (idx == 1) {
+                    continue;
+                }
+
+                String[] tokens = line.split(cvsSplitBy);
+                Book book = new Book();
+                book.setId(NumberUtils.toInt(tokens[0]));
+                book.setName(tokens[1]);
+                book.setPages(NumberUtils.toInt(tokens[2]));
+                book.setAuthor(tokens[3]);
+                book.setYear(tokens[4]);
+                if (book.getYear().isEmpty()) {
+                    book.setYear("N/A");
+                }
+                book.setPreview(tokens[5]);
+                book.setPublication(DateUtil.strToDate(tokens[6]));
+                books.add(book);
+            }
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
 
 
-			while ((line = br.readLine()) != null) {
-				idx++;
-				if (idx == 1) {
-					continue;
-				}
-
-				String[] tokens = line.split(cvsSplitBy);
-				Book book = new Book();
-				book.setId(NumberUtils.toInt(tokens[0]));
-				book.setName(tokens[1]);
-				book.setPages(NumberUtils.toInt(tokens[2]));
-				book.setAuthor(tokens[3]);
-				book.setYear(tokens[4]);
-				if (book.getYear().isEmpty()) {
-					book.setYear("N/A");
-				}
-				book.setPreview(tokens[5]);
-				book.setPublication(DateUtil.strToDate(tokens[6]));
-				books.add(book);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		return books;
 	}
 
