f1065996278f714ef5b313d7a36c4a8757f734bf
olegnovitskiy <oleg.novitskiy.dev@gmail.com>
2017-01-29 12:43:58 +0200

    Java 8 - Task 6: Custom Spliterator parallel (in progress)....

--- src/main/java/org/sample/CustomSpliterator.java
+++ src/main/java/org/sample/CustomSpliterator.java
@@ -2,14 +2,22 @@ package org.sample;
 
 import java.util.Spliterator;
 import java.util.function.Consumer;
-import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.StreamSupport;
 
-public class CustomSpliterator implements Spliterator<Integer> {
-    private int last = 1;
+public class CustomSpliterator implements Spliterator<Long> {
+    private long last;
+    private Long start = 1L;
+    private Long end = null;
+
+    public CustomSpliterator(Long start, Long end) {
+        this.start = start;
+        this.end = end;
+        this.last = start;
+    }
 
     @Override
-    public boolean tryAdvance(Consumer<? super Integer> action) {
+    public boolean tryAdvance(Consumer<? super Long> action) {
         if (action == null)
             throw new NullPointerException();
 
@@ -21,13 +29,14 @@ public class CustomSpliterator implements Spliterator<Integer> {
         return true;
     }
 
-    private boolean isPrime(int elem) {
-        return !IntStream.rangeClosed(2, elem / 2).anyMatch(elem1 -> elem % elem1 == 0);
+    private boolean isPrime(long elem) {
+        return !LongStream.rangeClosed(2, elem / 2).anyMatch(elem1 -> elem % elem1 == 0);
     }
 
     @Override
-    public Spliterator<Integer> trySplit() {
-        return null;
+    public Spliterator<Long> trySplit() {
+        Spliterator<Long> result = new CustomSpliterator(this.start, this.end == null?1000:(end-start)/2+start);
+        return result;
     }
 
     @Override
@@ -41,6 +50,6 @@ public class CustomSpliterator implements Spliterator<Integer> {
     }
 
     public static void main(String[] args) {
-        StreamSupport.stream(CustomSpliterator::new,0,false).limit(10).forEach(System.out::println);
+        StreamSupport.stream(()->new CustomSpliterator(1L, null),0,false).parallel().limit(1000).forEach(System.out::println);
     }
 }
